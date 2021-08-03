e98f2310e95a102e2fe1ac5ab9d404d1a6dd890a
Algacyr Melo <algacyr.junior@gmail.com>
2021-08-03 20:57:51 -0300

    creating transaction rows at DOM

--- scripts/finance.js
+++ scripts/finance.js
@@ -1,3 +1,16 @@
+const transactions = [
+  {
+    title: "Pizza",
+    date: "2021-08-01",
+    amount: "-51"
+  },
+  {
+    title: "Bolsa estágio",
+    date: "2021-08-03",
+    amount: "510"
+  }
+]
+
 function openModal() {
   $('#add-transaction-modal').modal('show')
 }
@@ -16,5 +29,47 @@ function addTransaction(event) {
     amount: transactionAmount
   }
 
-  console.log(transaction)
+  transactions.push(transaction)
+  App.reload()
+
+  //console.log(transactions)
 }
+
+function createTransactionRow(transaction, index) {
+  const transactionRow = document.createElement('tr')
+  transactionRow.dataset.index = index
+
+  transactionRow.innerHTML = `
+    <td>${index + 1}</td>
+    <td>${transaction.title}</td>
+    <td>${transaction.date}</td>
+    <td class=${Number(transaction.amount) > 0 ? 'income' : 'outcome'}>R$ ${transaction.amount}</td>
+  `
+
+  const transactionsContainer = document.querySelector('#transactions-container')
+  transactionsContainer.appendChild(transactionRow)
+}
+
+function clearTransactions() {
+  const transactionsContainer = document.querySelector('#transactions-container')
+  transactionsContainer.innerHTML = ""
+}
+
+const App = {
+  init() {
+    transactions.forEach((transaction, index) => {
+      createTransactionRow(transaction, index)
+    })
+  },
+
+  reload() {
+    clearTransactions()
+    this.init()
+  }
+
+}
+
+App.init()
+
+
+
