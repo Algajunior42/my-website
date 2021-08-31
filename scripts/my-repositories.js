86a5225116b63a98d445d7f28f7496b5040a0813
Algacyr Melo <algacyr.junior@gmail.com>
2021-08-30 22:31:08 -0300

    get my github repositories to a list

--- scripts/my-repositories.js
+++ scripts/my-repositories.js
@@ -1,4 +1,17 @@
-// Get github data
+const repositoryList = document.querySelector('#list-root')
+
+
 fetch('https://api.github.com/users/algadev/repos')
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(myRepositories => {
+    myRepositories.forEach((repository) => {
+      const repositoryItem = document.createElement('li')
+      repositoryItem.innerHTML = `
+      <h2>${repository.name}</h2>
+      <p>${repository?.description}</p>
+      <a href=${repository.html_url} target='_blank'>See on Github</a>
+      `
+
+      repositoryList.appendChild(repositoryItem)
+    })
+  })
