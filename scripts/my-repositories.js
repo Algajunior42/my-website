0ff6bf2c3c25c27cbe25783ff5be65ba2a5eb14b
Algacyr Melo <algacyr.junior@gmail.com>
2021-08-30 23:44:52 -0300

    fix scroll issue

--- scripts/my-repositories.js
+++ scripts/my-repositories.js
@@ -8,7 +8,7 @@ fetch('https://api.github.com/users/algadev/repos')
       const repositoryItem = document.createElement('li')
       repositoryItem.innerHTML = `
       <h2>${repository.name}</h2>
-      <p>${repository?.description}</p>
+      <p>${repository?.description || ''}</p>
       <a href=${repository.html_url} target='_blank'>See on Github</a>
       `
 
